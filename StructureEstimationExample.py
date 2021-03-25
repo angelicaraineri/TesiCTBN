553640e68fa1f5b40bdd8f354b872cadeda4c5f4
angelica <angelica.raineri@gmail.com>
2021-03-25 14:57:25 +0100

    Modifica f1 score

--- StructureEstimationExample.py
+++ StructureEstimationExample.py
@@ -10,36 +10,37 @@ from PyCTBN import StructureConstraintBasedEstimator
 def main():
    
     # <read the json files in ./data path>
-    read_files = glob.glob(os.path.join('./data/networks_and_trajectories_ternary_data_01_3', "2.json"))
+    read_files = glob.glob(os.path.join('./data/networks_and_trajectories_ternary_data_01_3', "*.json"))
     # <initialize a JsonImporter object for the first file>
     
-    importer = JsonImporter(file_path=read_files[0], samples_label='samples',
-                            structure_label='dyn.str', variables_label='variables',
-                            time_key='Time', variables_key='Name')
-    # <import the data at index 0 of the outer json array>
-    importer.import_data(0)
-    strut = importer._raw_data[0]['dyn.str']
-    with open('./output/realStructure.json', 'w') as f:
-        json.dump(strut, f)
+    for i in range(1,11):
+        importer = JsonImporter(file_path=read_files[i], samples_label='samples',
+                                structure_label='dyn.str', variables_label='variables',
+                                time_key='Time', variables_key='Name')
+        # <import the data at index 0 of the outer json array>
+        importer.import_data(0)
+        strut = importer._raw_data[0]['dyn.str']
+        with open('./output/realStructure%s.json' %i, 'w') as f:
+            json.dump(strut, f)
     
-    # construct a SamplePath Object passing a filled AbstractImporter object
-    s1 = SamplePath(importer=importer)
-    # build the trajectories
-    s1.build_trajectories()
-    # build the information about the net
-    s1.build_structure()
-    # construct a StructureEstimator object passing a correctly build SamplePath object and the
-    # independence tests significance, if you have prior knowledge about the net structure create a list of tuples
-    # that contains them and pass it as known_edges parameter
-    se1 = StructureConstraintBasedEstimator(sample_path=s1, exp_test_alfa=0.1, chi_test_alfa=0.1,
-                                            known_edges=[], thumb_threshold=25)
-    # call the algorithm to estimate the structure
-    se1.estimate_structure()
-    # obtain the adjacency matrix of the estimated structure
-    print(se1.adjacency_matrix())
-    # save the estimated structure  to a json file (remember to specify the path AND the .json extension)....
-    completeName =  os.path.join('./output' , "estimateStructure.json")
-    se1.save_results(completeName)
-    # ...or save it also in a graphical model fashion (remember to specify the path AND the .png extension)
-    se1.save_plot_estimated_structure_graph(os.path.join('./output' , "estimateStructure.png"))
+        # construct a SamplePath Object passing a filled AbstractImporter object
+        s1 = SamplePath(importer=importer)
+        # build the trajectories
+        s1.build_trajectories()
+        # build the information about the net
+        s1.build_structure()
+        # construct a StructureEstimator object passing a correctly build SamplePath object and the
+        # independence tests significance, if you have prior knowledge about the net structure create a list of tuples
+        # that contains them and pass it as known_edges parameter
+        se1 = StructureConstraintBasedEstimator(sample_path=s1, exp_test_alfa=0.1, chi_test_alfa=0.1,
+                                                known_edges=[], thumb_threshold=25)
+        # call the algorithm to estimate the structure
+        se1.estimate_structure()
+        # obtain the adjacency matrix of the estimated structure
+        print(se1.adjacency_matrix())
+        # save the estimated structure  to a json file (remember to specify the path AND the .json extension)....
+        completeName =  os.path.join('./output' , "estimateStructure%s.json" %i)
+        se1.save_results(completeName)
+        # ...or save it also in a graphical model fashion (remember to specify the path AND the .png extension)
+        se1.save_plot_estimated_structure_graph(os.path.join('./output' , "estimateStructure.png"))
 
--- f1score_calculate.py
+++ f1score_calculate.py
@@ -2,54 +2,63 @@ import json
 from sklearn.metrics import f1_score
 import pandas as pd
 
-with open("./output/estimateStructure.json") as f:
-    data = json.load(f)
-    
-array = data["links"]
-estimateLinks = []
+for i in range(1,11):
 
-for x in range(len(array)):
-    estimateLinks.append(array[x]["source"] + array[x]["target"])
-    
-
-with open("./output/realStructure.json") as f:
-    data = json.load(f)
-    
-realLinks = []
-
-for x in range(len(data)):
-    realLinks.append(data[x]["From"] + data[x]["To"])
-    
-estimateLinks = sorted(estimateLinks)
-
-realLinks = sorted(realLinks)
-    
-y_real = [1]*len(realLinks)
-y_pred = []
-
-
-for x in range(len(realLinks)):
-    if realLinks[x] in estimateLinks:
-        y_pred.append(1)
-    else:
-        y_pred.append(0)
+    with open("./output/estimateStructure%s.json" %i) as f:
+        data = json.load(f)
         
-if len(realLinks)>len(estimateLinks):
-    y_real.append(1)
-    y_pred.append(0)
-
-if len(realLinks)<len(estimateLinks):
-    y_real.append(0)
-    y_pred.append(1)
-       
-
-y_actu = pd.Series(y_real, name='Actual')
-y_pred2 = pd.Series(y_pred, name='Predicted')
-df_confusion = pd.crosstab(y_actu, y_pred2)
-
-f1_scor = f1_score(y_actu, y_pred2)
-
-print("F1 score : {}".format(f1_scor))
-
-with open('./output/f1score.json', 'w') as f:
-        json.dump(f1_scor, f)
+    array = data["links"]
+    estimateLinks = []
+    
+    for x in range(len(array)):
+        estimateLinks.append(array[x]["source"] + array[x]["target"])
+        
+    
+    with open("./output/realStructure%s.json" %i) as f:
+        data = json.load(f)
+        
+    realLinks = []
+    
+    for x in range(len(data)):
+        realLinks.append(data[x]["From"] + data[x]["To"])
+        
+    estimateLinks = sorted(estimateLinks)
+    
+    realLinks = sorted(realLinks)
+        
+    y_real = [1]*len(realLinks)
+    y_pred = []
+    
+    
+    for x in range(len(realLinks)):
+        if realLinks[x] in estimateLinks:
+            y_pred.append(1)
+        else:
+            y_pred.append(0)
+            
+    if len(realLinks)>len(estimateLinks):
+        y_real.append(1)
+        y_pred.append(0)
+    
+    if len(realLinks)<len(estimateLinks):
+        y_real.append(0)
+        y_pred.append(1)
+           
+    
+    y_actu = pd.Series(y_real, name='Actual')
+    y_pred2 = pd.Series(y_pred, name='Predicted')
+    df_confusion = pd.crosstab(y_actu, y_pred2)
+    
+    f1_scor = f1_score(y_actu, y_pred2)
+    
+    print("F1 score : {}".format(f1_scor))
+    
+    with open('./output/f1score%s.json' %i, 'w') as f:
+            json.dump(f1_scor, f)
+            
+f1_tot = 0
+for i in range(1, 11):
+   with open('./output/f1score%s.json' %i, 'w') as f:
+        data = json.load(f)
+        f1_tot += data
+    
