0f74a7e082ffedec1e19b0c6a2fd9077a95c1b14
Angelica Raineri <angelica.raineri@gmail.com>
2021-04-21 00:24:25 +0200

    BIG COMMIT

--- metricsCalculator.py
+++ metricsCalculator.py
@@ -1,97 +1,167 @@
 import yaml
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statistics
+from operator import itemgetter
 import json
 from sklearn.metrics import f1_score
 import pandas as pd
+import numpy as np
+from itertools import chain
 
-f1_tot=0
-json_data_to_plot = {}
+
 json_data = {}
+list_variables = []
+binary = []
+ternary = []
+quaternary = []
+    
+def adj_list_to_adj_matrix_real(adj_list, variables):
+    #print("REAL MATRIX " , adj_list)
+    variables = pd.DataFrame(variables)
+    adj_matrix = np.zeros((variables.shape[0], variables.shape[0]))
+    for edge in adj_list:
+        adj_matrix[variables[variables["Name"] == edge["From"]].index[0],variables[variables["Name"] == edge["To"]].index[0]] = 1
+    return adj_matrix
+
+
+def adj_list_to_adj_matrix_estimate(adj_list, variables):
+    #print("ESTIMATE MATRIX " , adj_list)
+    variables = pd.DataFrame(variables)
+    adj_matrix = np.zeros((variables.shape[0], variables.shape[0]))
+    for edge in adj_list:
+        adj_matrix[variables[variables["Name"] == edge[0]].index[0],variables[variables["Name"] == edge[1]].index[0]] = 1
+    return adj_matrix
+
+def confusion_matrix(real, predicted):
+    p_minus_t = predicted - real
+    
+    global fp
+    fp = np.sum(p_minus_t > 0)
+    global fn
+    fn = np.sum(p_minus_t < 0)
+    global tp
+    tp = np.sum(real == 1) - fn
+    global tn
+    tn = np.sum(real == 0) - fn
+        
+    ret = np.array([[tp, fp] , [fn, tn]], dtype = int)
+    return ret
+
+def plot_dataset(data_x_axis, data_y_axis,density, cardinality):
+    dataset = pd.DataFrame({'f1 score': data_y_axis, 'number of variables' : data_x_axis})
+    ax = sns.lineplot( x = 'number of variables' , y = 'f1 score', marker = 'o', data = dataset)
+    ax.set_xticks(data_x_axis)
+    ax.set_title(cardinality+" data with 0"+str(density)+" density and "+ \
+str(number_trajectories)+" indipendent trajectories")
+    name = cardinality+"_0"+str(density)
+    ax = ax.get_figure()
+    ax.savefig("output/Graphs/%s.pdf" %name)
+    ax.clf()
+
+def build_dataset(data_to_plot):
+    data_x_axis.append(data_to_plot.pop(0))
+    data_y_axis.append(statistics.mean(data_to_plot))
+    return data_x_axis, data_y_axis
+
+#array_nodes = [[3], [4], [5], [6], [10], [15], [20]]
+array_nodes = [[3], [4], [5], [6], [10]]
+
+for i in range(3): # cardinality = 01, 02, 03, 04
+    binary.append([])
+    ternary.append([]) 
+    quaternary.append([])
+    for t in range(len(array_nodes)):
+        binary[i].append(list(array_nodes[t]))
+        ternary[i].append(list(array_nodes[t]))
+        quaternary[i].append(list(array_nodes[t]))
+
 with open('params.yaml') as file:
-   documents = yaml.full_load(file)
-   number_trajectories = documents['feature']['number_trajectories']
-   number_variables = documents['feature']['number_variables']
+    documents = yaml.full_load(file)
+    number_trajectories = documents['feature']['number_trajectories']
 
-
-for i in range(1,11):
-
-    with open("./output/estimateStructure.json") as f:
-        data = json.load(f)
+with open("./output/estimateStructure.json") as f:
+    estimate_data = json.load(f)
         
-    array = data["links"]
-    estimateLinks = []
-    
-    for x in range(len(array)):
-        estimateLinks.append(array[x]["source"] + array[x]["target"])
-        
-    
-    with open("./output/Structure/realStructure%s.json" %i) as f:
-        data = json.load(f)
-        
-    realLinks = []
-    
-    for x in range(len(data)):
-        realLinks.append(data[x]["From"] + data[x]["To"])
-    
-    y_real = [1]*len(realLinks)
-    y_pred = []
-    
-    
-    for x in range(len(realLinks)):
-        if realLinks[x] in estimateLinks:
-            y_pred.append(1)
-        else:
-            y_pred.append(0)
-            
-    if len(realLinks)>len(estimateLinks):
-        y_real.append(1)
-        y_pred.append(0)
-    
-    if len(realLinks)<len(estimateLinks):
-        y_real.append(0)
-        y_pred.append(1)
-           
-    
-    y_actu = pd.Series(y_real, name='Actual')
-    y_pred = pd.Series(y_pred, name='Predicted')
-    df_confusion = pd.crosstab(y_pred, y_actu)
-    print(df_confusion)
-    f1 = f1_score(y_actu, y_pred)    
-    print("F1 score : {}".format(f1))
+with open("./output/realStructure.json") as f:
+    real_data = json.load(f)
 
-    json_data['File %s' %i] = {
-      'F1 score' : f1,
-      'Numero of nodes' : number_nodes,
+        
+for item in estimate_data:
+    #print(" ")
+    print(item)
+    struct_estimate = estimate_data.get(item)
+    struct_real = real_data.get(item)[0]
+    variables = real_data.get(item)[1]
+
+    conf = confusion_matrix(adj_list_to_adj_matrix_real(struct_real, variables),\
+            adj_list_to_adj_matrix_estimate(struct_estimate, variables))
+    
+    #print("CONFUSION MATRIX : " , conf)
+
+    recall = tp / (tp + fn)
+    precision = tp / (tp + fp)
+    f1_score = 2*((precision*recall)/(precision + recall))
+    #print("F1 SCORE " , f1_score)
+    
+    cardinality = item.split("/")[2].split("_")[3]
+    
+    if (len(item.split("/")[2].split("_")) < 7):
+        density = 3
+        nodes = int(item.split("/")[2].split("_")[5])
+    else:
+        density = int((item.split("/")[2].split("_")[5])[1])
+        nodes = int(item.split("/")[2].split("_")[6])
+
+    if (nodes <= 6):
+        index = nodes - 3
+    elif (nodes == 10):
+        index = 4
+    elif (nodes == 15):
+        index = 5
+    else:
+        index = 6
+
+    if(cardinality == "binary"):
+        binary[density - 1][index].append(f1_score)
+    elif (cardinality == "ternary"):
+        ternary[density - 1][index].append(f1_score)
+    elif (cardinality == "quaternary"):
+        quaternary[density - 1][index].append(f1_score)
+                
+    json_data['File %s' %item] = {
+      'F1 score' : f1_score,
       'Numbero of indipendent trajectories' : number_trajectories,
-      'Confusion matrix' : df_confusion.to_json()
+      'Confusion matrix' : conf.tolist()
     }
-
-    with open('./output/metrics.json' , 'a') as f:
+    with open('./output/metrics.json' , 'w') as f:
         json.dump(json_data, f)
 
-    f1_tot += f1
-    
-f1_mean = f1_tot/10
+data_x_axis = []
+data_y_axis = []
 
-print("F1 medio : {}".format(f1_mean)) 
 
-json_data['Metrics generali'] = {
-   'F1 score mean' : f1_mean,
-   'Number of nodes' : number_nodes,
-   'Number of indipendent trajectories' : number_trajectories,
-}   
+for i in range(len(binary)):
+    for x in range(len(binary[i])): #all values for density = i
+        data_to_plot = list(binary[i][x])
+        result = build_dataset(data_to_plot)
+    plot_dataset(result[0], result[1], i + 1, "binary")
+    data_x_axis = []
+    data_y_axis = []
 
-with open('./output/metrics.json' , 'w') as f:
-	json.dump(json_data, f)
+for i in range(len(ternary)):
+    for x in range(len(ternary[i])): #all values for density = i
+        data_to_plot = list(ternary[i][x])
+        result = build_dataset(data_to_plot)
+    plot_dataset(result[0], result[1], i + 1, "ternary")
+    data_x_axis = []
+    data_y_axis = []
 
-json_data_to_plot = {
-    'F1 score' : f1_mean,
-    'Number of nodes' : number_nodes,
-}
+for i in range(len(quaternary)):
+    for x in range(len(quaternary[i])): #all values for density = i     
+        data_to_plot = list(quaternary[i][x])
+        result = build_dataset(data_to_plot)
+    plot_dataset(result[0], result[1], i + 1, "quaternary")
+    data_x_axis = []
+    data_y_axis = []
 
-data = json.load(open('./output/metrics_to_plot.json'))
-if type(data) is dict:
-    data = [data]
-data.append(json_data_to_plot)
-
-with open('./output/metrics_to_plot.json','w') as f:
-        json.dump(data, f)
--- structureEstimation.py
+++ structureEstimation.py
@@ -12,12 +12,11 @@ import itertools
 
 class main():
     
-    cont = 1
     y = []
     list_of_path = []
     list_of_combination = []
-    json_data = {}
-    json_data2 = {}
+    json_data_real = {}
+    json_data_est = {}
 
     with open('params.yaml') as file:
         documents = yaml.full_load(file)
@@ -34,18 +33,23 @@ class main():
         list_of_combination.append(element)
 
     for x in range(len(list_of_combination)):
-        path ='./data/networks_and_trajectories_' + list_of_combination[x][0] + '_data_0' + str(list_of_combination[x][1]) + '_'+ str(list_of_combination[x][2])
-        list_of_path.append(path)
-            
+        if list_of_combination[x][1]==3:
+             path ='./data/networks_and_trajectories_' + list_of_combination[x][0] + '_data_'+ str(list_of_combination[x][2])
+        else :
+            path ='./data/networks_and_trajectories_' + list_of_combination[x][0] + '_data_0' + str(list_of_combination[x][1]) + '_'+ str(list_of_combination[x][2])
+        if (path != './data/networks_and_trajectories_quaternary_data_01_20' and path != './data/networks_and_trajectories_quaternary_data_02_20' and path != './data/networks_and_trajectories_quaternary_data_20'):
+            list_of_path.append(path)
+               
     for x in range(len(list_of_path)):
         read_files = glob.glob(os.path.join(list_of_path[x], "*.json"))
         for i in range(0,10):
+            print(" ")
+            print(read_files[i])
             importer = JsonImporter(file_path=read_files[i], samples_label='samples',
                                 structure_label='dyn.str', variables_label='variables',
                                 time_key='Time', variables_key='Name')
         
             importer.import_data(0)
-            print(read_files[i])
 
             if (number_trajectories != 300):
                 samples = importer._raw_data[0]['samples']
@@ -54,10 +58,16 @@ class main():
                 importer._raw_data[0]['samples'] = newSamples
         
             strut = importer._raw_data[0]['dyn.str']
-            print(type(strut))
-            json_data['Struttura reale del file %s' %read_files[i]] =  strut
-            with open('./output/realStructure.json' , 'w') as f:
-                json.dump(json_data, f)
+            var = importer._raw_data[0]['variables']
+            
+
+            json_data_real['%s' %read_files[i]] = []
+            json_data_real['%s' %read_files[i]].append(strut)
+            json_data_real['%s' %read_files[i]].append(var)
+    
+            
+            #with open('./output/realStructure.json' , 'w') as f:
+                #json.dump(json_data, f)
     
             s1 = SamplePath(importer=importer)
             s1.build_trajectories()
@@ -66,11 +76,17 @@ class main():
                                                 known_edges=[], thumb_threshold=25)
             #se1.estimate_structure()
             #print(se1.adjacency_matrix())
-            json_data2['Struttura stimata del file %s' %read_files[i]] = list(se1.estimate_structure())
-            with open('./output/estimateStructure.json' , 'w') as f:
-                json.dump(json_data2, f)
+            
+            json_data_est['%s' %read_files[i]] = list(se1.estimate_structure())
+            #with open('./output/estimateStructure.json' , 'w') as f:
+                #json.dump(json_data2, f)
+            
             #completeName =  os.path.join('./output/Structure' , "estimateStructure%s.json" %cont)
             #se1.save_results(completeName)
             # se1.save_plot_estimated_structure_graph(os.path.join('./output' , "estimateStructure%s.png" %cont))
-            cont = cont+1
-    
+            #cont = cont+1
+    with open('./output/realStructure.json' , 'w') as f:
+        json.dump(json_data_real, f)
+
+    with open('./output/estimateStructure.json' , 'w') as f:
+        json.dump(json_data_est, f)
